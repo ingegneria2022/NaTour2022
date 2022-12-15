1b16098e00d5d72e4fe687b8f388036a1769ccb3
Giuseppe Bracale <103512836+Peppolino@users.noreply.github.com>
2022-12-15 13:06:25 +0100

    Add files via upload

---
+++ NaTourFE15-12-22/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "natour2122fe98851850": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "NaTour22GroupRole": "string",
+            "CognitoUsersPoolGroupNaTourGroupRole": "string"
+        }
+    }
+}
